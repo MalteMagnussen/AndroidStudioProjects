8373bb58ef40d900e576c56a972418911725e112
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-08-03 17:24:47 +0200

    Learning SafeArea and Container

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.deepPurpleAccent,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            color: Colors.white,
+            child: Text(
+              'Hello World',
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
