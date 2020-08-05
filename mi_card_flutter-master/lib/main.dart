0ab7a2f06ea737acf967da97582da9f535b0b58c
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-08-05 17:45:57 +0200

    Adding email and phone to micard

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -38,9 +38,51 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(10.0),
                 child: Row(
                   children: <Widget>[
-                    Icon(Icons.add_shopping_cart),
+                    Icon(
+                      Icons.add_shopping_cart,
+                      color: Colors.teal,
+                      //size: 50.0,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('+45 42 30 12 07',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontSize: 20.0,
+                            fontFamily: 'Source Sans Pro'))
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('maltehviidmagnussen@gmail.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro'))
                   ],
                 ),
               ),
